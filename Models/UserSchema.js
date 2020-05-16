79f8da4210b27ab178a1d137b47814d300d5735b
Rajat Gupta <rajat295gupta@gmail.com>
2020-05-16 11:21:08 +0530

    user schema

---
+++ Models/UserSchema.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+
+var userSchema = new Schema({					
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    role: {
+        type: String,
+    },   
+})
+
+module.exports =  mongoose.model('users', userSchema);
