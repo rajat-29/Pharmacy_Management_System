a6258a25011dde9ff2bdd814574ad10f32b1b2fa
Rajat Gupta <rajat295gupta@gmail.com>
2020-05-16 12:04:27 +0530

    logout

--- Routes/Handlers/login.js
+++ Routes/Handlers/login.js
@@ -1,6 +1,16 @@
+let express = require('express');
 var app = require('express').Router();
+let path = require('path');
+
+app.use(express.static(path.join(__dirname,'../../Public')));
 
 let loginController = require('../../Controllers/login');
+
+app.get('/logutUser', function(req,res) {
+    req.session.isLogin = 0;
+    req.session.destroy();
+    res.render('login');
+})
 
 // controllers //
 
